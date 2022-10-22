c620b84854811aef09d55d8e069de78001ddda69
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-22 17:13:52 -0600

    Allow a new application to be started/created

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -12,20 +12,15 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    applicant = Applicant.create!(applicant_params)
-    applicant.applications.create!(application_params)
+    Application.create!(application_params)
 
     redirect_to "/applications/#{Application.last.id}"
   end
 
   private
 
-  def applicant_params
-    params.permit(:name, :address, :city, :state, :zip_code)
-  end
-
   def application_params
-    params.permit(:description, :status, :submitted)
+    params.permit(:name, :address, :city, :state, :zip_code, :description, :status, :submitted)
   end
 
 end
